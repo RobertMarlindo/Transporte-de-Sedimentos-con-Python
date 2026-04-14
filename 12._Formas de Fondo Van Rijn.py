693f715c625a5b0eac04d50e7e3b5905554baec0
RobertMarlindo <68715176+RobertMarlindo@users.noreply.github.com>
2026-04-14 10:23:52 -0500

    Update 12._Formas de Fondo Van Rijn.py

--- 12._Formas de Fondo Van Rijn.py
+++ 12._Formas de Fondo Van Rijn.py
@@ -1,31 +1,31 @@
-#DESARROLLADORES: Ing. Ramirez Quispe, Robert Marlindo
-#               : Ing. Córdova Julca, Guillermo Arturo
-# PROGRAMA      : Formas de fondo - Van Rijn
-# LENGUAJE      : Python
+//DESARROLLADORES: Ing. Ramirez Quispe, Robert Marlindo
+//               : Ing. Córdova Julca, Guillermo Arturo
+// PROGRAMA      : Formas de fondo - Van Rijn
+// LENGUAJE      : Python
 from math import log10,sqrt
 print("============================")
 print("Formas de fondo")
 print("============================")
-#datos
-#=======
-y=2.7003            #tirante del agua m calculado con RCanales
-B=200               #base del canal
-v=1.073             #velocidad m/s obtenido con Rcanales
-Fr=0.2086           #Numero de froude de Rcanales opcional
-D50=0.35/1000       #diametro particula d50 m(metros) para micras multipicar 10^6
-D90=0.90/1000       #diametro particula d90 m(metros)
-ps=2650             #peso espeficifico del sólido kg/m3
-g=9.807             #gravedad m/s2
+//datos
+//=======
+y=2.7003            //tirante del agua m calculado con RCanales
+B=200               //base del canal
+v=1.073             //velocidad m/s obtenido con Rcanales
+Fr=0.2086           //Numero de froude de Rcanales opcional
+D50=0.35/1000       //diametro particula d50 m(metros) para micras multipicar 10^6
+D90=0.90/1000       //diametro particula d90 m(metros)
+ps=2650             //peso espeficifico del sólido kg/m3
+g=9.807             //gravedad m/s2
 Te=20
 R=B*y/(B+2*y)
-#calcula funcion de T O ingresar peso espeficifico del agua kg/m3
-u=(1.14-0.031*(Te-15)+0.00068*(Te-15)**2)*10**-6  #calcula funcion de T O viscosidad cinematica del fluido m2/s
+//calcula funcion de T O ingresar peso espeficifico del agua kg/m3
+u=(1.14-0.031*(Te-15)+0.00068*(Te-15)**2)*10**-6  //calcula funcion de T O viscosidad cinematica del fluido m2/s
 p=1000.*(1.-(Te+288.941)*pow(Te-3.986,2.)/(508929.2*(Te+68.13)))
-#Método de Van Rijn
-#calculo de parametro de shiedls
-#==== diagramas original =========
-pr=(ps-p)/p #densidad ro
-D=(pr*g/u**2)**(1/3)*D50 #D*
+//Método de Van Rijn
+//calculo de parametro de shiedls
+//==== diagramas original =========
+pr=(ps-p)/p //densidad ro
+D=(pr*g/u**2)**(1/3)*D50 //D*
 if D>1 and D<=4:
     te=.24*D**(-1)
 elif D>4 and D<=10:
@@ -36,8 +36,8 @@ elif D>20 and D<=150:
     te=.013*D**(.29)
 elif D>150:
     te=.055   
-Tb_cr=te*(ps-p)*g*D50    #esfuerzo cortante critico
-C=18*log10(12*R/(3*D90)) #parametro de chezy
+Tb_cr=te*(ps-p)*g*D50    //esfuerzo cortante critico
+C=18*log10(12*R/(3*D90)) //parametro de chezy
 T_bc=p*g*(v/C)**2
 T=(T_bc-Tb_cr)/Tb_cr
 print("Radio hidraulico R=",R,"m")
@@ -53,8 +53,8 @@ print("Esfuerzo cortante critico Tb,cr =",Tb_cr,"N/m2")
 print("18*log10(12*y/(3*D90)) C  =",C,"m^(1/2)/s")
 print("Esfuerzo cortante critico Tb,c =",T_bc,"N/m2")
 print("Valor de T  ",T)
-#busca de la tabla
-#lower  = abajo
+//busca de la tabla
+//lower  = abajo
 if T>=0 and T<=3:
     if D>=1 and D<=10:
         print(" Pequeño megarizos 0<T<3; 1<D*<10")
@@ -70,10 +70,10 @@ elif T>10 and T<=15:
         print(" Dunas 10<T<15; 1<D*<10")
     elif D>10:
         print(" Dunas 10<T<15; D*>10")       
-#transition
+//transition
 if T>15 and T<=25:
     print(" Dunas lavadas, olas de san 15<T<25")   
-#uppe r= arriba
+//uppe r= arriba
 if T>=25 and Fr<0.8:
     print(" olas simétricas de arena T>=25, Fr<0.8")
     print(" (symmetrical) sand waves")
